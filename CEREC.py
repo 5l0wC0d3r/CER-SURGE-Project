494b758301af7c3011cb510c3f18a848a2689d0c
5l0wC0d3r <105980346+5l0wC0d3r@users.noreply.github.com>
2023-06-09 21:36:28 +0530

    Finished code for DERC

--- CEREC.py
+++ CEREC.py
@@ -1,8 +1,43 @@
+import csv
 from selenium import webdriver
-import pandas as pd
 from bs4 import BeautifulSoup
 
-def CERC_FETCH():
-    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-    driver.get("https://cercind.gov.in/recent_orders.html")
-    
+# Set up Selenium webdriver with Chrome
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+# Navigate to the website
+driver.get('https://cercind.gov.in/recent_orders.html')
+
+# Wait for the page to load (adjust the sleep duration if needed)
+driver.implicitly_wait(5)
+
+# Get the page source after waiting for JavaScript rendering
+html = driver.page_source
+
+# Create a BeautifulSoup object to parse the HTML
+soup = BeautifulSoup(html, 'html.parser')
+
+# Find the table containing the orders
+table = soup.find('table', class_='display')
+
+# Create a CSV file to store the data
+with open('orders.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+    writer.writerow(['Order Number', 'Order Date', 'Description'])
+
+    # Iterate over the rows in the table (excluding the header row)
+    rows = table.find_all('tr')[1:]
+    for row in rows:
+        # Extract the data from each column
+        columns = row.find_all('td')
+        order_number = columns[0].text.strip()
+        order_date = columns[1].text.strip()
+        description = columns[2].text.strip()
+
+        # Write the data to the CSV file
+        writer.writerow([order_number, order_date, description])
+
+# Close the Selenium webdriver
+driver.quit()
